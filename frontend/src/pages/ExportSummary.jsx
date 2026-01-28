862285330dcf205414f1d5fa49d079daca583d88
Mohammad Maroof <mohdmaroof12340@gmail.com>
2026-01-29 01:01:23 +0530

    Update ExportSummary.jsx

--- frontend/src/pages/ExportSummary.jsx
+++ frontend/src/pages/ExportSummary.jsx
@@ -1,3 +1,5 @@
+
+import { API_BASE } from "../config";
 import React, { useState } from 'react';
 import { Box, Typography, Button, Paper, CircularProgress, Fade, Grow, Grid, Alert } from '@mui/material';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
